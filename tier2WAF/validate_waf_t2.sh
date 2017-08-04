ef125b57971258d9db7a98ada0e077bc3d981ae3
Mike Shimkus <m.shimkus@f5.com>
2017-08-04 12:32:39 -0700

    updated validate waf

--- tier2WAF/validate_waf_t2.sh
+++ tier2WAF/validate_waf_t2.sh
@@ -5,13 +5,15 @@ function passwd() {
   echo | awk '{print $1}' /config/mypass
 }
 
+dfl_mgmt_port=`tmsh list sys httpd ssl-port | grep ssl-port | sed 's/ssl-port //;s/ //g'`
+
 # check that the data groups have been populated
 failed=0
 until [[ $record_count -ge "6" ]] || [[ $failed -eq "30" ]]; do
      failed=$(($failed + 1))
      record_count=0
      for group in applianceid appmap eventhub; do
-          records=$(curl -sku admin:$(passwd) -X GET -H "Content-Type: application/json" https://localhost/mgmt/tm/ltm/data-group/internal/${group}_datagroup | jq .records | jq length)
+          records=$(curl -sku admin:$(passwd) -X GET -H "Content-Type: application/json" https://localhost:$dfl_mgmt_port/mgmt/tm/ltm/data-group/internal/${group}_datagroup | jq .records | jq length)
           (( record_count += records ))
      done
      sleep 10
@@ -26,7 +28,7 @@ fi
 
 # check that the remote logging pool is populated
 failed=0
-until [[ "$(curl -sku admin:$(passwd) -X GET -H "Content-Type: application/json" https://localhost/mgmt/tm/ltm/pool/~Common~azure-log.app~azure-log_remote/members | jq -r .items[0].fqdn.autopopulate | grep "enabled")" ]] || [[ $failed -eq "30" ]]; do
+until [[ "$(curl -sku admin:$(passwd) -X GET -H "Content-Type: application/json" https://localhost:$dfl_mgmt_port/mgmt/tm/ltm/pool/~Common~azure-log.app~azure-log_remote/members | jq -r .items[0].fqdn.autopopulate | grep "enabled")" ]] || [[ $failed -eq "30" ]]; do
      failed=$(($failed + 1))
      sleep 10
 done
@@ -39,10 +41,17 @@ else
 fi
 
 # disable eventhub datagroup icall handler
-response_code=$(curl -sku admin:$(passwd) -w "%{http_code}" -X PUT -H "Content-Type: application/json" https://localhost/mgmt/tm/sys/icall/handler/periodic/eventhub_datagroup -d '{"status": "inactive", "interval": "10",  "script": "eventhub_datagroup"}' -o /dev/null)
+response_code=$(curl -sku admin:$(passwd) -w "%{http_code}" -X PUT -H "Content-Type: application/json" https://localhost:$dfl_mgmt_port/mgmt/tm/sys/icall/handler/periodic/eventhub_datagroup -d '{"interval": "300",  "script": "eventhub_datagroup"}' -o /dev/null)
 
 if [[ $response_code != 200  ]]; then
-     echo "Failed to disable eventhub iCall handler with response code '"$response_code"'"
+     echo "Failed to update eventhub iCall handler with response code '"$response_code"'"
+fi
+
+# save the config
+response_code=$(curl -sku admin:$(passwd) -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost:$dfl_mgmt_port/mgmt/tm/sys/config -d '{"command": "save"}' -o /dev/null)
+
+if [[ $response_code != 200  ]]; then
+     echo "Failed to save the configuration with response code '"$response_code"'"
 fi
 
 echo "Verification complete."
